98536eecb8f40bd4380f4f0553a530af0789eecd
Jhojahn ramirez <jhojahn@gmail.com>
2026-03-04 13:30:04 +0100

    style: change board separator style

--- src/Board.java
+++ src/Board.java
@@ -19,7 +19,7 @@ public class Board {
         for (int i=0; i<3; i++){
             System.out.println(" "+board[i][0]+ " | "+ board[i][1]+ " | "+ board[i][2]+ " ");
             if (i<2){
-                System.out.println("*========*");
+                System.out.println("==========");
             }
         }
     }
