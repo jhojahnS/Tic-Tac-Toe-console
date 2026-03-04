8ad12e920ba8fe6b7f50cc29441c877ad0eb52ef
Jhojahn ramirez <jhojahn@gmail.com>
2026-03-04 12:43:42 +0100

    feat: add board display functionality

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,9 @@
 public class Game {
 
     public static void main(String []args){
-        System.out.println("el juego");
+
+        Board board= new Board();
+        board.printBoard();
+
     }
 }
