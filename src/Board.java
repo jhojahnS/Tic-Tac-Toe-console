541ac691a648c642134d5fa1d8dd7becdb654f6e
Jhojahn ramirez <jhojahn@gmail.com>
2026-03-04 17:51:34 +0100

    feat: detect draw

--- src/Board.java
+++ src/Board.java
@@ -50,4 +50,14 @@ public class Board {
 
         return false;
     }
+    public boolean isFull(){
+        for (int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(board[i][j]==' '){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
